0bf148e6766c301eb5cf725187ffaad4f8e53e17
tanveer <tanveerdedhaka@gmail.com>
2023-03-08 17:56:40 +0600

    scrapy project 1 inititated

---
+++ web_scrapping_projects/worldometer/worldometer/spiders/countries.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class CountriesSpider(scrapy.Spider):
+    name = "countries"
+    allowed_domains = ["www.worldometers.info"]
+    start_urls = ["https://www.worldometers.info/"]
+
+    def parse(self, response):
+        pass
